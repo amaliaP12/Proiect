49feb8c105f09e10e0ef6d80882df0b4c2646249
amaliaP12 <amaliapetre04@gmail.com>
2025-01-04 15:50:02 +0200

    am editat

--- SistemECommerce/SistemECommerce/Utilizator.cs
+++ SistemECommerce/SistemECommerce/Utilizator.cs
@@ -5,16 +5,13 @@ public class Utilizator
     public int Id { get; set; }
     public string Nume { get; set; }
     public string Email { get; set; }
-    public string Parola { get; set; }
-    public string Rol { get; set; } = "Client";
 
-    public Utilizator(int id, string nume, string email, string parola, string rol)
+    public Utilizator(int id, string nume, string email)
     {
         Id = id;
         Nume = nume;
         Email = email;
-        Parola = parola;
-        Rol = rol;
+       
     }
     
 }
