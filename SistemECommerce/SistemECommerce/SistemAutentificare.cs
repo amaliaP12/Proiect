ec1361fceebf073b6569ca5a758e56dd93443986
pClaudiaA <postu.andreea.claudia@gmail.com>
2025-01-17 10:37:51 +0200

    am adăugat comentarii la clasele mele: utilizator, administrator si sistem autentificare; și la clasele amaliei: aplicatie, comanda, datamanager,produs, reducere si usermanager deoarece ei ia bubuit laptopul

--- SistemECommerce/SistemECommerce/SistemAutentificare.cs
+++ SistemECommerce/SistemECommerce/SistemAutentificare.cs
@@ -2,52 +2,58 @@ namespace SistemECommerce;
 
 public class SistemAutentificare
 {
-
+    //lista utilizatorilor inregistratți în sistem
     private List<Utilizator> utilizatori;
 
+    //constructor inițializare sistem autentificare
     public SistemAutentificare()
     {
+        //inițializare listă utilizatori
         utilizatori = new List<Utilizator>();
     }
 
+    //functie inregistrare utilizatori noi
     public void InregistreazaUtilizator(Utilizator utilizator)
     {
+        //verifica existența unui utilizator cu același email
         if (utilizatori.Exists(u => u.Email == utilizator.Email))
         {
             Console.WriteLine("Un utilizator cu acest email există deja.");
         }
         else
         {
+            //adaugare utilizator in lista
             utilizatori.Add(utilizator);
             Console.WriteLine("Utilizator înregistrat cu succes.");
         }
     }
     
-
+    //functie autentificare pe baza email si parolă
     public Utilizator? Autentifica(string email, string parola)
     {
+        //incarcare utilizatori din UserManager
         UserManager.IncarcaUtilizatori(); 
-
+        //cautare client in functie de email si parola
         var client = UserManager.Clienti.FirstOrDefault(c => 
             c.Email.Equals(email, StringComparison.OrdinalIgnoreCase) && c.VerificaParola(parola));
 
         if (client != null)
         {
             Console.WriteLine($"Autentificat ca Client: {client.Nume}");
-            return client;
+            return client;//returnare client autentificat
         }
-
+        //cautare ]n listă administratori dupa email si parola
         var admin = UserManager.Administratori.FirstOrDefault(a =>
             a.Email.Equals(email, StringComparison.OrdinalIgnoreCase) && a.VerificaParola(parola));
 
         if (admin != null)
         {
             Console.WriteLine($"Autentificat ca Administrator: {admin.Nume}");
-            return admin;
+            return admin;//returnare administrator autentificat
         }
-
+        
         Console.WriteLine("Autentificare esuata! Verifica datele introduse.");
-        return null;
+        return null;//daca utilizatorul nu a fost gasit se va returna null
     }
 
 }
